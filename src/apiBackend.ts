b886cfdf2f02ee880ae955bfe4242e8a2f6c02e0
Cristian Cobaxin <cdcs030600@gmail.com>
2025-02-24 17:10:49 -0600

    mod for FullStack prod

--- src/apiBackend.ts
+++ src/apiBackend.ts
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 // Create an instance of axios with default configuration
 const apiAuth = axios.create({
-  baseURL: 'http://localhost:8000/api/auth/',
+  baseURL: 'https://library-laravel-main-cnots4.laravel.cloud/api/auth/',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 const apiBook = axios.create({
-  baseURL: 'http://localhost:8000/api/auth/',
+  baseURL: 'https://library-laravel-main-cnots4.laravel.cloud/api/auth/',
   headers: {
     'Content-Type': 'multipart/form-data',
     'Accept': 'application/json'
